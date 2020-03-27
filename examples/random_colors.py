626b8522c7b6dc65d5f5e299fc5f7cdd89167d34
Conrad <conradstorz@gmail.com>
2020-03-26 21:58:52 -0400

    cast RGB value list as tuple

--- examples/random_colors.py
+++ examples/random_colors.py
@@ -87,8 +87,9 @@ def UseLumaLEDMatrix(device, x, y, pace, rounds):
         iters = field[pixel_x * 8 + pixel_y]
         field[pixel_x * 8 + pixel_y] = iters - 1
         #senseObj.set_pixel(pixel_x, pixel_y, color_dict[color]["rgb"])
+        color_tuple = tuple(color_dict[color]["rgb"])
         with canvas(device) as draw:
-                    draw.point((pixel_x, pixel_y), color_dict[color]["rgb"])
+                    draw.point((pixel_x, pixel_y), color_tuple)
         delay = (sum(field) / rounds) / (100 / pace)
         if delay > 0:
             sleep(delay)
