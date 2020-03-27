773bde181fefb86a044209c46bb24d6947a80874
Conrad <conradstorz@gmail.com>
2020-03-26 22:17:34 -0400

    incr

--- examples/random_colors.py
+++ examples/random_colors.py
@@ -80,21 +80,18 @@ def UseLumaLEDMatrix(device, x, y, pace, rounds):
     if type(y) != list:
         y = list(range(y))
     field = [int(rounds) for i in range(len(x) * len(y))]
-    with canvas(device) as draw:
-        while sum(field) > -(rounds*100): # extend run time 
-            color = choice(COLOR_KEYS)
-            pixel_x = choice(x)
-            pixel_y = choice(y)
-            iters = field[pixel_x * 8 + pixel_y]
-            field[pixel_x * 8 + pixel_y] = iters - 1
-            #senseObj.set_pixel(pixel_x, pixel_y, color_dict[color]["rgb"])
-            color_tuple = tuple(color_dict[color]["rgb"])
-            draw.point((pixel_x, pixel_y), color_tuple)
-            delay = (sum(field) / rounds) / (100 / pace)
-            if delay > 0:
-                sleep(delay)
-            else:
-                sleep(.0001)
+    while sum(field) > -(rounds*100): # extend run time
+        with canvas(device) as draw:
+            for i in range(256):
+                color = choice(COLOR_KEYS)
+                pixel_x = choice(x)
+                pixel_y = choice(y)
+                iters = field[pixel_x * 8 + pixel_y]
+                field[pixel_x * 8 + pixel_y] = iters - 1
+                #senseObj.set_pixel(pixel_x, pixel_y, color_dict[color]["rgb"])
+                color_tuple = tuple(color_dict[color]["rgb"])
+                draw.point((pixel_x, pixel_y), color_tuple)
+
     return color
 
 
