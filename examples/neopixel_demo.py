1871a2a12cd9d8166dfd158a3f06457c8c25cb71
Conrad <conradstorz@gmail.com>
2020-04-09 23:32:35 -0400

    changed framerate to 3/sec

--- examples/neopixel_demo.py
+++ examples/neopixel_demo.py
@@ -236,7 +236,8 @@ def set_bounds_limits(r, g, b):
     b = int(max(0, min(255, b)))
     return (r, g, b)
 
-FRAMERATE = 0.083333333  # approx 12 frames/second
+
+FRAMERATE = 0.333333333
 
 def gfx(device):
     effects = [glitter, tunnel, rainbow_search, checker, swirl, blues_and_twos]
